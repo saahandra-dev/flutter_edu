e4945fee30cfe2536b5d6744981fda0a0437595b
saahandraa <sandraemenikee@gmail.com>
2022-07-11 18:07:51 +0100

    config

---
+++ e_commerce/lib/size_config.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static MediaQueryData ? __mediaQueryData;
+  static double ? screenWidth;
+  static double ? screenHeight;
+  static double ? defaultSize;
+  static Orientation ? orientation;
+
+  void init(BuildContext context) {
+    __mediaQueryData = MediaQuery.of(context);
+    screenWidth = __mediaQueryData!.size.width;
+    screenHeight = __mediaQueryData!.size.height;
+    orientation = __mediaQueryData!.orientation;
+  }
+
+}
+
+double getProportionateScreenHeight(double inputHeight) {
+  double ? screenHeight = SizeConfig.screenHeight;
+  
+  return (inputHeight / 812.0) * screenHeight!;
+}
+double getProportionateScreenWidth(double inputWidth) {
+  double ? screenWidth = SizeConfig.screenWidth;
+  
+  return (inputWidth / 812.0) * screenWidth!;
+}
