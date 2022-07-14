6d6d2b7bc2fcb078bee006201e2c6cc0d18688dc
saahandraa <sandraemenikee@gmail.com>
2022-07-14 21:39:29 +0100

    route

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/route.dart';
 import 'package:e_commerce/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: SplashScreen(),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
     );
   }
 }
